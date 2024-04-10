10fe86a77f6eeb51164cde3eb5a3086048363808
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-04-10 16:09:37 +0100

    create the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./App.css";
 import Routing from "./Routing";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ui/theme-provider";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
-        <Routing />
+        <div className="flex flex-col gap-3">
+          <Navbar />
+
+          <Routing />
+        </div>
       </BrowserRouter>
     </ThemeProvider>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,39 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { TfiTarget } from "react-icons/tfi";
+
 function Navbar() {
   return (
-    <div>
-      <div>faz</div>
+    <div className="py-2 px-1  border-b border-b-slate-900 ">
+      <div className="max-w-7xl mx-auto flex items-center gap-4 ">
+        <div className="flex items-center gap-2 font-4 ">
+          <TfiTarget className="text-lg text-red-500" />
+
+          <p className="font-bold  text-lg">Crypto Scope</p>
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Portfolio</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>stuff</NavigationMenuLink>
+                <NavigationMenuLink>stuff</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <NavigationMenuLink>Documentation</NavigationMenuLink>
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </div>
   );
 }
