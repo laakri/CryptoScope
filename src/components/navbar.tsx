178bb1b5eb8f06006ca2c09ca3c26250fae2c76e
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-04-10 21:03:17 +0100

    update the homepage ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,38 +1,32 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
 import { TfiTarget } from "react-icons/tfi";
-
+import { Button } from "./ui/button";
 function Navbar() {
   return (
-    <div className="py-2 px-1  border-b border-b-slate-900 ">
-      <div className="max-w-7xl mx-auto flex items-center gap-4 ">
+    <div className="py-3 px-1  border-b border-b-slate-900  ">
+      <div className="max-w-7xl mx-auto flex justify-between ">
         <div className="flex items-center gap-2 font-4 ">
-          <TfiTarget className="text-lg text-red-500" />
-
-          <p className="font-bold  text-lg">Crypto Scope</p>
+          <TfiTarget className="text-lg text-gray-300" />
+          <p className="font-bold  text-md ">Crypto Scope</p>
         </div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Portfolio</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>stuff</NavigationMenuLink>
-                <NavigationMenuLink>stuff</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>
-                <NavigationMenuLink>Documentation</NavigationMenuLink>
-              </NavigationMenuTrigger>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="flex items-center gap-4 ">
+          <div>
+            <Button variant="ghost" className=" text-gray-300">
+              Portfolio
+            </Button>
+            <Button variant="ghost" className=" text-gray-300">
+              Documentation
+            </Button>
+            <Button variant="ghost" className=" text-gray-300">
+              Informations
+            </Button>
+            <Button variant="ghost" className=" text-gray-300">
+              Dashboard
+            </Button>
+          </div>
+          <div className="flex gap-2 ">
+            <Button variant="ghost">Connect Wallet</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function HomePage() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="  flex min-h-60  items-center justify-center px-16">
+      <div className="relative w-full max-w-lg">
+        <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl "></div>
+        <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl "></div>
+        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl "></div>
+        <div className="m-8 relative space-y-4">
+          <div className="p-5  rounded-lg flex items-center justify-between space-x-8">
+            <Input type="email" placeholder="Email" />
+            <Button type="submit">Subscribe</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
