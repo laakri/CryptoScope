c00fcc8a8b1043660f5bfe484e7b3c131adeb072
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2024-04-25 09:24:15 +0100

    update the homep page adding some motion frames ui

--- apps/client/src/PublicPages/Homepage.tsx
+++ apps/client/src/PublicPages/Homepage.tsx
@@ -1,56 +1,51 @@
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaSearch } from "react-icons/fa";
 
 function HomePage() {
   return (
-    <div className="max-w-7xl mx-auto w-full h-[40rem]   flex justify-between items-center p-6 ">
-      <div className="flex flex-col gap-6 ">
-        <h1 className="text-5xl max-w-[46rem]">
-          Explore more with Crypto Scope
-        </h1>
-        <p className="text-gray-300  max-w-[32rem]">
-          Explore more with Crypto Scope more with Crypto Scope Explore more
-          with Crypto Scope Explore more with Crypto Scope
-        </p>
-        <div className="relative w-96 ">
-          <Input
-            type="text"
-            placeholder="Search For Coin by name"
-            className="h-10 rounded-2xl"
-          />
-          <p className="text-sm text-muted-foreground absolute top-2 right-2">
-            <Button className="h-6 rounded-lg border bg-muted hover:bg-slate-700 px-2 text-[10px] text-muted-foreground">
-              <FaSearch />
-            </Button>
-          </p>
+    <div className="overflow-hidden">
+      <BackgroundGradientAnimation>
+        <div className="max-w-7xl mx-auto w-full h-[40rem]   flex justify-center items-center p-6 z-10">
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <h1 className="text-7xl max-w-[46rem] text-center">
+              Explore more with Crypto Scope
+            </h1>
+            <p className="text-gray-300  max-w-[32rem] text-center ">
+              Explore more with Crypto Scope more with Crypto Scope Explore more
+              with Crypto Scope Explore more with Crypto Scope
+            </p>
+            <div className="relative w-96 ">
+              <Input
+                type="text"
+                placeholder="Search For Coin by name"
+                className="h-10 rounded-2xl"
+              />
+              <p className="text-sm text-muted-foreground absolute top-2 right-2">
+                <Button className="h-6 rounded-lg border bg-muted hover:bg-slate-700 px-2 text-[10px] text-muted-foreground">
+                  <FaSearch />
+                </Button>
+              </p>
+            </div>
+            <h1>Trending</h1>
+            <div className="flex flex-wrap space-x-2  ">
+              <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
+                BTC $69,335.05
+              </div>
+              <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
+                ETH $3,521.91
+              </div>
+              <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
+                Solana $0.05981
+              </div>
+              <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
+                MAGIC $1.05
+              </div>
+            </div>
+          </div>
         </div>
-        <h1>Trending</h1>
-        <div className="flex flex-wrap space-x-2  ">
-          <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
-            BTC $69,335.05
-          </div>
-          <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
-            ETH $3,521.91
-          </div>
-          <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
-            Solana $0.05981
-          </div>
-          <div className="border-2 border-gray-900 p-2 max-w-max rounded-xl ">
-            MAGIC $1.05
-          </div>
-        </div>
-      </div>
-      <div className=" px-16">
-        <div className="relative w-full max-w-lg">
-          <div className="absolute top-0 -left-4 w-96 h-96  bg-pink-900 rounded-full opacity-30 filter blur-xl animate-blob"></div>
-          <div className="absolute top-0 -right-4 w-96 h-96 bg-yellow-900 rounded-full opacity-30 filter blur-xl  animate-blob animation-delay-2000"></div>
-          <div className="absolute -bottom-8 left-0 w-96 h-96 bg-pink-900 rounded-full  opacity-30 filter blur-xl  animate-blob animation-delay-4000"></div>
-          <div className="absolute -bottom-8 right-0 w-96 h-96 bg-purple-900 rounded-full  opacity-30 filter blur-xl  animate-blob animation-delay-4000"></div>
-          <div className="m-8 relative space-y-4"></div>
-        </div>
-        <div className="max-h-1 max-w-1"></div>
-      </div>
+      </BackgroundGradientAnimation>
     </div>
   );
 }
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -62,6 +62,39 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -90,6 +123,11 @@ module.exports = {
         },
       },
       animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
         blob: "blob 7s infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
