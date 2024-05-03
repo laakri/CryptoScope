c6d4f22dffc9a35f9dc5fe57fc8700e3b6190651
Firas Jaber <firasjaberc@gmail.com>
2024-05-03 17:43:09 +0100

    basic db setup with prisma and lucia

--- apps/tapi/src/context.ts
+++ apps/tapi/src/context.ts
@@ -1,5 +1,9 @@
 import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
 
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
 export interface User {
   name: string[] | string;
 }
@@ -7,7 +11,7 @@ export interface User {
 export function createContext({ req, res }: CreateFastifyContextOptions) {
   const user: User = { name: req.headers.username ?? 'anonymous' };
 
-  return { req, res, user };
+  return { req, res, user, db: client };
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
