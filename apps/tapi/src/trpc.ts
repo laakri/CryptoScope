c17a9bb70f80cb024e473eb5f379ad690e833b0f
Firas Jaber <firasjaberc@gmail.com>
2024-05-05 14:23:16 +0200

    refactor to multiple routers and add protected routes example for user

--- apps/tapi/src/trpc.ts
+++ apps/tapi/src/trpc.ts
@@ -1,6 +1,7 @@
-import { initTRPC } from '@trpc/server';
+import { TRPCError, initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 import { Context } from './context.js';
+import { lucia } from './lucia.js';
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
@@ -11,3 +12,37 @@ const t = initTRPC.context<Context>().create({
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+const isAuthed = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.req.headers.authorization) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'No Authorization bearer',
+    });
+  }
+
+  // get user session with lucia and validate it
+  const authorizationHeader = ctx.req.headers.authorization;
+  const sessionId = lucia.readBearerToken(authorizationHeader ?? '');
+  if (!sessionId) {
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Invalid session' });
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+
+  console.log('user:', user);
+
+  if (!user) {
+    lucia.invalidateSession(sessionId);
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Invalid session' });
+  }
+
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...session, user: user },
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(isAuthed);
